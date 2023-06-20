df8735f8f598f32e5967e7614c9aa2993c0b8593
tennjugu <133909875+tennjugu@users.noreply.github.com>
2023-06-20 16:41:49 +0100

    add getPlayerChoice to index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function getComputerChoice(choices) {
     const choiceIndex = Math.floor(Math.random () * choicesLength)
     return choices[choiceIndex]
 }
-const choices= ["Rock", "Papper", "Scissors"]
+const choices= ["rock", "paper", "scissors"]
 const choicesLength = choices.length
-console.log(getComputerChoice(choices))
+//console.log(getComputerChoice(choices))
+
+let getPlayerChoice = prompt("Pick between rock, paper or scissors")
+console.log(getPlayerChoice.toLowerCase())
+if (getPlayerChoice.toLowerCase() != "rock" && getPlayerChoice.toLowerCase() != "paper" && getPlayerChoice.toLowerCase() != "scissors")
+alert("Your choice was not rock, paper or scissors. Try again")
+
+
