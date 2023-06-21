86c4054b971ba539239151afb63ec86848191844
tennjugu <133909875+tennjugu@users.noreply.github.com>
2023-06-21 11:38:00 +0100

    add playerSelection and computerSelection

--- index.js
+++ index.js
@@ -25,18 +25,26 @@ function playRound(playerSelection, computerSelection) {
     return ("scissors beats paper, player wins!")}
 
 }
-const playerSelection = getPlayerChoice.toLowerCase()
-const computerSelection = getComputerChoice(choices)
-console.log(playRound(playerSelection, computerSelection))
+
 
 function game() {
     let computerPoint = 0
     let playerPoint = 0
     for (let i = 0; i < 5; i++) {
         const getPlayerChoice = prompt("Pick between rock, paper or scissors")
-        console.log(getPlayerChoice.toLowerCase())
+        //console.log(getPlayerChoice.toLowerCase())
         if (getPlayerChoice.toLowerCase() != "rock" && getPlayerChoice.toLowerCase() != "paper" && getPlayerChoice.toLowerCase() != "scissors")
         alert("Your choice was not rock, paper or scissors. Try again")
+
+        const playerSelection = getPlayerChoice.toLowerCase()
+        const computerSelection = getComputerChoice(choices)
+        console.log (`player selection : ${playerSelection}`)
+        console.log (`computer selection: ${computerSelection}`)
+        console.log(`Round: ${i +1}`)
+
+        const round = playRound(playerSelection,computerSelection)
+        console.log(round)
     }
 
 }
+game()
